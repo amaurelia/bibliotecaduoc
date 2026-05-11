a6e8e38f60464eea95a4463984f5179810ed6dc1
amaurelia <seba4to@gmail.com>
2026-05-11 00:04:42 -0400

    se agrega spring security

---
+++ src/main/java/com/example/bibliotecaduoc/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.bibliotecaduoc.repository;
+
+import com.example.bibliotecaduoc.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
+}
